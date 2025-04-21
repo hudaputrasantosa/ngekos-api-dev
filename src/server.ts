6e8b9014debd1f988cf50ff29b6fd5f16463705e
hudaputrasantosa <hudaputrasantosa@gmail.com>
2025-04-21 21:45:49 +0700

    fix: add env variable, and update general file

--- src/server.ts
+++ src/server.ts
@@ -35,16 +35,16 @@ app.use('/', (_req, res) => {
 });
 app.use(router);
 
-// app.use((err: ErrorWithStatus, req: Request, res: Response, next: NextFunction): any => {
-//   const errorStatus = err.status ?? 500;
-//   const errorMessage = err.message || 'Something went Wrong!';
-//   return res.status(errorStatus).json({
-//     success: false,
-//     status: errorStatus,
-//     message: errorMessage,
-//     stack: process.env.NODE_ENV === 'development' ? err.stack : undefined,
-//   });
-// });
+app.use((err: ErrorWithStatus, req: Request, res: Response, next: NextFunction): any => {
+  const errorStatus = err.status ?? 500;
+  const errorMessage = err.message || 'Something went Wrong!';
+  return res.status(errorStatus).json({
+    success: false,
+    status: errorStatus,
+    message: errorMessage,
+    stack: process.env.NODE_ENV === 'development' ? err.stack : undefined,
+  });
+});
 
 const PORT: number = 8800;
 
